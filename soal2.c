833f6a5ae683a2066fb4af09810dedbe47ac797d
hisyamzf <55182043+hisyamzf@users.noreply.github.com>
2020-03-13 17:45:22 +0700

    Update and rename soal2 to soal2.c

---
+++ soal2.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <time.h>
+#include <wait.h>
+#include <string.h>
+#define SIZE 1000
+
+
+
+     int main() {
+  pid_t child;
+  time_t rawtime;
+  int status;
+  struct tm *timeinfo;
+  char file[SIZE];
+  char aa[SIZE];
+  char place[SIZE];
+    rawtime = time(NULL);
+  timeinfo = localtime ( &rawtime ); 
+  strcpy (file, "/home/osboxes/sisop20/");
+  strftime (aa, SIZE, "%Y-%m-%d_%H:%M:%S", timeinfo);
+  strcat (file, aa);
+     child = fork();
+   if (child == 0) {
+     sleep(30);
+     char *argv[] = {"mkdir","-p",file,NULL};
+     execv("/bin/mkdir", argv);
+   } 
+}
+
+
